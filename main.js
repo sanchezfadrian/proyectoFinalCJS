fd094beae78cb4d44dbde5c26c377fb227dac262
sanchezfadrian <sanchezfadrian@gmail.com>
2023-09-02 20:08:29 -0300

    Entrega Proyecto Final

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-//Array para ir guardando las reservas
+const valorDesayuno = 2
+const valorCochera = 5
+
 const reservas = []
 
-//Objeto que me permite guardar todos los datos necesarios para efectuar la reserva
-const reserva = {
+const datosReserva = {
     idReserva: null,
     idCabaña: null,
     fechaInicio: null,
@@ -17,9 +18,8 @@ const reserva = {
     valorTotal: null
 }
 
-//Clase que me permite crear objetos del tipo cabaña con la información de cada una de ellas
 class Cabaña {
-    constructor(nombre, cantidadAdultos, cantidadMenores, valorNoche) {
+    constructor(nombre, cantidadAdultos, cantidadMenores, valorNoche){
         this.nombre = nombre
         this.cantidadAdultos = cantidadAdultos
         this.cantidadMenores = cantidadMenores
@@ -27,16 +27,13 @@ class Cabaña {
     }
 }
 
-// Cabañas creadas
-const cabaña1 = new Cabaña("Cabaña Estandar S (2 personas)", 2, 0, 15000)
-const cabaña2 = new Cabaña("Cabaña Superior S (2 personas)", 2, 0, 20000)
-const cabaña3 = new Cabaña("Cabaña Estandar M (4 personas)", 2, 2, 25000)
-const cabaña4 = new Cabaña("Cabaña Superior M (6 personas)", 2, 4, 30000)
+const cabaña1 = new Cabaña("Cabaña Estandar S (2 personas)", 2, 0, 75)
+const cabaña2 = new Cabaña("Cabaña Superior S (2 personas)", 2, 0, 150)
+const cabaña3 = new Cabaña("Cabaña Estandar M (4 personas)", 2, 2, 100)
+const cabaña4 = new Cabaña("Cabaña Superior M (6 personas)", 2, 4, 200)
   
-// Array de cabañas compuesto por objetos del tipo cabaña
 const cabañas = [cabaña1, cabaña2, cabaña3, cabaña4]
 
-//Obtencion DOM
 const fechaInicio = document.getElementById("fechaInicio")
 const fechaFin = document.getElementById("fechaFin")
 const cantidadAdultos = document.getElementById("cantidadAdultos")
@@ -45,159 +42,256 @@ const desayuno = document.getElementById("desayuno")
 const cochera = document.getElementById("cochera")
 const nombre = document.getElementById("nombre")
 const email = document.getElementById("email")
+const valor = document.getElementById("valor")
 
-const cardCabaña1 = document.getElementById("cardCabaña1")
-const cardCabaña2 = document.getElementById("cardCabaña2")
-const cardCabaña3 = document.getElementById("cardCabaña3")
-const cardCabaña4 = document.getElementById("cardCabaña4")
-
-const btnCabaña1 = document.getElementById("btnCabaña1")
-const btnCabaña2 = document.getElementById("btnCabaña2")
-const btnCabaña3 = document.getElementById("btnCabaña3")
-const btnCabaña4 = document.getElementById("btnCabaña4")
-const btnReserva = document.getElementById("btnReserva")
-const btnRecuperar = document.getElementById("btnRecuperar")
 const btnBorrar = document.getElementById("btnBorrar")
+const btnRecuperar = document.getElementById("btnRecuperar")
+const btnReserva = document.getElementById("btnReserva")
+const btnVerificar = document.getElementById("btnVerificar")
+const formulario = document.getElementById("formulario")
+
+const botonesCabaña = document.querySelectorAll('.btnCabaña')
+const tarjetasCabaña = document.querySelectorAll('.tarjetaCabaña')
 
 btnReserva.addEventListener("click",crearReserva)
-btnRecuperar.addEventListener("click",recuperarReserva)
-btnBorrar.addEventListener("click",borrarReserva)
+btnBorrar.addEventListener("click",borrarDatos)
+btnRecuperar.addEventListener("click",recuperarDatos)
 
-btnCabaña1.onclick = () => seleccionCabaña(0)
-btnCabaña2.onclick = () => seleccionCabaña(1)
-btnCabaña3.onclick = () => seleccionCabaña(2)
-btnCabaña4.onclick = () => seleccionCabaña(3)
+function seleccionCabaña(numeroTarjeta) {
+    datosReserva.idCabaña = numeroTarjeta
+    tarjetasCabaña.forEach((tarjeta, index) => {
+        tarjeta.classList.remove('seleccionado')
+        if (index === numeroTarjeta) {
+            tarjeta.classList.add('seleccionado')
+        }
+    })
+}
 
-// En esta funcion se validan los datos que van dentro del objeto reserva para que no haya inconsistencias
-function crearReserva(e){
+botonesCabaña.forEach((button, index) => {
+    button.addEventListener('click', () => {
+        seleccionCabaña(index)
+    })
+})
+
+async function crearReserva(e) {
     e.preventDefault()
+    if (validarDatos()) {
+        cargarDatos()
+        const esValida = await validarReserva()
+        if (esValida) {
+            guardarStorage(datosReserva)
+            reservas.push(datosReserva)
+            console.log(reservas)
+            mensajeAlerta("Reserva efectuada exitosamente", "success")
+        }
+    }
+}
+
+btnVerificar.addEventListener('click', function() {
+    Swal.fire({
+        title: 'Reserva',
+        input: 'text',
+        inputLabel: 'Valor:',
+        inputPlaceholder: 'Ingrese el codigo de reserva proporcionado',
+        showCancelButton: true,
+        inputValidator: (value) => {
+            if (!value) {
+                return 'Debe ingresar un valor'
+            }
+        }
+    }).then((result) => {
+        if (result.isConfirmed) {
+            const idBuscado = result.value
+            const indice = reservas.findIndex(obj => obj.idReserva === idBuscado)
+
+            if (indice !== -1) {
+                Swal.fire({
+                    title: 'Valor encontrado',
+                    html: `<h2># ${reservas[indice].idReserva}</h2><br><b>Ingreso:</b> ${reservas[indice].fechaInicio} - <b>Egreso:</b> ${reservas[indice].fechaFin}<br><b>Cantidad de dias</b> ${reservas[indice].cantidadDias} - <b>Adultos:</b> ${reservas[indice].cantidadAdultos} - <b>Menores:</b> ${reservas[indice].cantidadMenores}<br><b>Cochera:</b> ${reservas[indice].cochera} - <b>Desayuno:</b> ${reservas[indice].desayuno}<br><b>Nombre:</b> ${reservas[indice].nombre} - <b>Correo:</b> ${reservas[indice].email}<br><br><h3>Valor total: US$ ${reservas[indice].valorTotal}`,
+                    icon: 'success'
+                })
+            } else {
+                Swal.fire({
+                    title: 'Valor no encontrado',
+                    text: `El valor "${idBuscado}" no se encuentra, contactenos para verificar el problema.`,
+                    icon: 'error'
+                })
+            }
+        }
+    })
+})
+
+function validarDatos(){
     if (!validarCabaña()) return false
-    if (!validarFecha(fechaInicio)) return false
-    if (!validarFecha(fechaFin)) return false
-    if (!validarNumero(cantidadAdultos,1,cabañas[reserva.idCabaña].cantidadAdultos)) return false
-    if (!validarNumero(cantidadMenores,0,cabañas[reserva.idCabaña].cantidadMenores)) return false
+    if (!validarFechas(fechaInicio,fechaFin)) return false
+    if (!validarNumero(cantidadAdultos,cabañas[datosReserva.idCabaña].cantidadAdultos)) return false
+    if (!validarNumero(cantidadMenores,cabañas[datosReserva.idCabaña].cantidadMenores)) return false
+    if (!validarSeleccion(desayuno)) return false
+    if (!validarSeleccion(cochera)) return false
     if (!validarTexto(nombre)) return false
     if (!validarTexto(email)) return false
-    reserva.idReserva = generarIdReserva()
-    reserva.fechaInicio = fechaInicio.value
-    reserva.fechaFin = fechaFin.value
-    reserva.cantidadAdultos = cantidadAdultos.value
-    reserva.cantidadMenores = cantidadMenores.value
-    reserva.desayuno = desayuno.value
-    reserva.cochera = cochera.value
-    reserva.nombre = nombre.value
-    reserva.email = email.value
-    reserva.cantidadDias = obtenerDiferenciaDias(new Date(fechaInicio.value),new Date(fechaFin.value))
-    reserva.valorTotal =  ObtenerValorTotal(reserva.cantidadDias,cabañas[reserva.idCabaña].valorNoche,0,0)
-    guardarStorage(reserva) //guardo en el storage por si se produce un error de conexion con el servidor al momento de ejecutar la reserva
-    reservas.push(reserva)
-    alert(`La reserva Nº ${reserva.idReserva} se ha ejecutado con exito, Su estadia comienza ${reserva.fechaInicio} y finaliza el ${reserva.fechaFin} con un total de ${reserva.cantidadDias} noche/s. El total a abonar es de $ ${reserva.valorTotal}, los datos de la transferencia seran enviados al correo ${reserva.email}. Muchas gracias`)
-    borrarReserva()
+    return true
 }
 
-function borrarReserva(e){
-    cardCabaña0.classList.remove("unSelect")
-    cardCabaña1.classList.remove("unSelect")
-    cardCabaña2.classList.remove("unSelect")
-    cardCabaña3.classList.remove("unSelect")
-    reserva.idCabaña = null
+function cargarDatos(){
+    datosReserva.idReserva = generarIdReserva()
+    datosReserva.fechaInicio = fechaInicio.value
+    datosReserva.fechaFin = fechaFin.value
+    datosReserva.cantidadAdultos = parseInt(cantidadAdultos.value)
+    datosReserva.cantidadMenores = parseInt(cantidadMenores.value)
+    datosReserva.desayuno = desayuno.value
+    datosReserva.cochera = cochera.value
+    datosReserva.nombre = nombre.value
+    datosReserva.email = email.value
+    datosReserva.cantidadDias = obtenerIntervalo(new Date(fechaInicio.value),new Date(fechaFin.value))
+    datosReserva.valorTotal =  obtenerValorTotal()
 }
 
-// Validamos si se ha seleccionado una cabaña
+function borrarDatos(e){
+    formulario.reset()
+    seleccionCabaña(-1)
+}
+
 function validarCabaña(){
-    if(reserva.idCabaña !== null) return true
-    alert(`Debe seleccionar una cabaña para continuar`)
+    if(datosReserva.idCabaña !== null) return true
+    mensajeAlerta('Debe seleccionar una cabaña para continuar','error')
     return false
 }
 
-// Validamos el formato de fecha correcto
-function validarFecha(fecha){
-    if (!isNaN(Date.parse(fecha.value))) return true
-    fecha.focus()
-    alert(`La fecha ingresada no es válida. Intente nuevamente`)
-    return false
-}
-
-// Validamos que el formato sea numerico y que este entre un maximo y un minimo
-function validarNumero(numero,minimo,maximo){
-    let cantidad = parseInt(numero.value)
-    if(cantidad>=minimo && cantidad<= maximo) return true
-    numero.focus()
-    alert(`Ingrese un valor numerico valido entre ${minimo} y ${maximo}`)
-    return false
-}
-
-// Validamos si existe un valor distinto de vacio
-function validarTexto(texto){
-    if ((texto.value).trim() !== "") return true
-        texto.focus()
-        alert("Por favor ingrese un valor valido para este campo")
+function validarFechas(fechaInicio, fechaFin){
+    const fechaInicioValue = Date.parse(fechaInicio.value)
+    const fechaFinValue = Date.parse(fechaFin.value)
+    const fechaActual = new Date()
+    if (!fechaInicioValue) {
+        fechaInicio.focus()
+        mensajeAlerta('Debe seleccionar una fecha de Chek-In válida', 'error')
         return false
+    }
+    if (fechaInicioValue <= fechaActual) {
+        fechaInicio.focus()
+        mensajeAlerta('Debe seleccionar una fecha Chek-In mayor a la fecha actual', 'error')
+        return false
+    }
+    if (!fechaFinValue) {
+        fechaFin.focus()
+        mensajeAlerta('Debe seleccionar una fecha de Chek-Out válida', 'error')
+        return false
+    }
+    if (fechaFinValue <= fechaInicioValue) {
+        fechaFin.focus()
+        mensajeAlerta('Debe seleccionar una fecha de Check-out mayor a la fecha de inicio', 'error')
+        return false
+    }
+    return true
 }
 
-//Obtenemos la diferencia de dias entre el inicio y fin
-function obtenerDiferenciaDias(fechaInicio,fechaFin) {
+function validarNumero(numero,maximo) {
+    let cantidad = parseInt(numero.value)
+    if(cantidad<= maximo) return true
+    numero.focus()
+    mensajeAlerta('Maximo de ocupantes para esta cabaña superado, seleccione otra cabaña','error')
+    return false
+}
+
+function validarTexto(texto) {
+    if ((texto.value).trim() !== "") return true
+    texto.focus()
+    mensajeAlerta('Por favor ingrese un valor valido para este campo','error')
+    return false
+}
+
+function validarSeleccion(seleccion) {
+    if(seleccion.value !== "") return true
+    seleccion.focus()
+    mensajeAlerta('Por favor seleccione un valor valido para este campo','error')
+    return false
+}
+
+async function validarReserva() {
+    try {
+        const valorDolar = await obtenerValorDolar()
+        const result = await Swal.fire({
+            title: 'Validacion de datos',
+            html: `<h2># ${datosReserva.idReserva}</h2><br><b>Ingreso:</b> ${datosReserva.fechaInicio} - <b>Egreso:</b> ${datosReserva.fechaFin}<br><b>Cantidad de dias</b> ${datosReserva.cantidadDias} - <b>Adultos:</b> ${datosReserva.cantidadAdultos} - <b>Menores:</b> ${datosReserva.cantidadMenores}<br><b>Cochera:</b> ${datosReserva.cochera} - <b>Desayuno:</b> ${datosReserva.desayuno}<br><b>Nombre:</b> ${datosReserva.nombre} - <b>Correo:</b> ${datosReserva.email}<br><br><h3>Valor total: US$ ${datosReserva.valorTotal}</h3><h5>Valor total: $ ${datosReserva.valorTotal * valorDolar}</h5><br><i>La información para realizar la transferencia será enviada al correo indicado</i>`,
+            icon: 'info',
+            showDenyButton: true,
+            confirmButtonText: 'Confirmar reserva',
+            denyButtonText: `Cancelar`,
+        })
+        
+        if (result.isConfirmed) {
+            return true
+        } else {
+            return false
+        }
+    } catch (error) {
+        console.error("Ocurrió un error:", error)
+        return false
+    }
+}
+
+function obtenerIntervalo(fechaInicio,fechaFin) {
     const diaMilisegundos = 24 * 60 * 60 * 1000
     const diferenciaMilisegundos = fechaFin - fechaInicio
     const cantidadDias = Math.floor(diferenciaMilisegundos / diaMilisegundos)
     return cantidadDias
 }
 
-//Obtenemos el valor total (el desayuno y la cochera lo dejamos con un valor fijo en esta entrega)
-function ObtenerValorTotal(dias, valor, desayuno, cochera){
-    return dias * (valor + desayuno + cochera)
+function obtenerValorTotal() {
+    const totalCochera = 0
+    const totalDesayuno = 0
+    const cantidadDias = datosReserva.cantidadDias
+    const desayuno = datosReserva.desayuno
+    const cochera = datosReserva.cochera
+    const valorNoche = cabañas[datosReserva.idCabaña].valorNoche
+    const cantidadPersonas = datosReserva.cantidadAdultos + datosReserva.cantidadMenores
+
+    if(desayuno === "SI") totalDesayuno = valorDesayuno * cantidadDias * cantidadPersonas
+    if(cochera === "SI") totalCochera = valorCochera * cantidadDias
+    return (valorNoche * cantidadDias + totalDesayuno + totalCochera)
 }
 
-
-function generarIdReserva(){
-    return Math.floor(Math.random() * (9999 - 1000)) + 1000
+function generarIdReserva() {
+    return "RES" + Math.floor(Math.random() * (9999 - 1000)) + 1000
 }
 
-function guardarStorage(reserva){
-    const reservaJson = JSON.stringify(reserva)
-    localStorage.setItem("reserva",reservaJson)
+function guardarStorage(datosReserva){
+    const reservaJson = JSON.stringify(datosReserva)
+    localStorage.setItem("datosReserva",reservaJson)
 }
 
-function recuperarReserva(e){
-    e.preventDefault()
-    const reservaJson = localStorage.getItem("reserva")
+function recuperarDatos(e) {
+    const reservaJson = localStorage.getItem("datosReserva")
     const reservaParseada = JSON.parse(reservaJson)
     fechaInicio.value = reservaParseada.fechaInicio
     fechaFin.value = reservaParseada.fechaFin
     cantidadAdultos.value = reservaParseada.cantidadAdultos
     cantidadMenores.value = reservaParseada.cantidadMenores
+    desayuno.value = reservaParseada.desayuno
     cochera.value = reservaParseada.cochera
     nombre.value = reservaParseada.nombre
     email.value = reservaParseada.email
     seleccionCabaña(parseInt(reservaParseada.idCabaña))
 }
 
-function seleccionCabaña(idCabaña){
-    reserva.idCabaña = idCabaña
-    cardCabaña0.classList.remove("unSelect")
-    cardCabaña1.classList.remove("unSelect")
-    cardCabaña2.classList.remove("unSelect")
-    cardCabaña3.classList.remove("unSelect")
-    switch (idCabaña) {
-        case 0:
-            cardCabaña1.classList.add("unSelect")
-            cardCabaña2.classList.add("unSelect")
-            cardCabaña3.classList.add("unSelect")
-            break
-        case 1:
-            cardCabaña0.classList.add("unSelect")
-            cardCabaña2.classList.add("unSelect")
-            cardCabaña3.classList.add("unSelect")
-            break
-        case 2:
-            cardCabaña0.classList.add("unSelect")
-            cardCabaña1.classList.add("unSelect")
-            cardCabaña3.classList.add("unSelect")
-            break
-        case 3:
-            cardCabaña0.classList.add("unSelect")
-            cardCabaña1.classList.add("unSelect")
-            cardCabaña2.classList.add("unSelect")
-            break
-      }
+function mensajeAlerta(mensaje,icono) {
+    Swal.fire({
+        text: mensaje,
+        icon: icono,
+        confirmButtonText: 'OK'
+    })
+}
+
+function obtenerValorDolar() {
+    return fetch("https://api.bluelytics.com.ar/v2/latest")
+        .then(response => response.json())
+        .then(data => {
+            const resultado = data.oficial
+            const valorCompra = resultado.value_buy
+            return valorCompra
+        })
+        .catch(error => {
+            console.error("Ocurrió un error:", error)
+            throw error
+        })
 }
